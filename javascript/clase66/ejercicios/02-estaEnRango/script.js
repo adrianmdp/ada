fc8740900b66f33c3d4539f2fbff8f2fbd3d577f
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 00:51:33 -0300

    Add ej 02

---
+++ javascript/clase66/ejercicios/02-estaEnRango/script.js
@@ -0,0 +1,2 @@
+// Crear una función estaEnRango que tome como argumentos tres números, un valor, un número minimo y un número maximo, y devuelva true si el valor se encuentra entre los números minimo y maximo o false si no (si el valor es igual a uno de los extremos se considera que está dentro del rango)
+
