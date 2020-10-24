9f78783730e9cc2f6428e2939a54baf9a85cf017
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 14:24:16 -0300

    add ej 15

---
+++ javascript/clase66/ejercicios/15-obtenerNota/script.js
@@ -0,0 +1,8 @@
+// Crear una función obtenerNota que tome como argumento un número puntaje y devuelva un string dependiendo del puntaje redondeado, con las siguientes reglas:
+
+// Menor a 6	                        => Desaprobado
+// Mayor o igual a 6 y menor a 7	    => Regular
+// Mayor o igual a 7 y menor a 8	    => Bueno
+// Mayor o igual a entre 8 y menor a 10 => Muy bueno
+// 10	                                => Excelente
+// Menor a 0 o mayor a 10	            => Puntaje inválido
