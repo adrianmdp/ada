266c92d2aaf4df82d22cce07d1802effbf325b91
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:04:57 -0300

    Add ej 17

---
+++ javascript/clase66/ejercicios/17-Cajero/script.js
@@ -0,0 +1,2 @@
+// Crear un programa que muestre el dinero inicial, y que permita retirar dinero preguntando cuánto se desea retirar, y mostrando el dinero restante a continuación. Si se intenta retirar más dinero del disponible, o no cuenta con dinero disponible, debe mostrar un mensaje alertándolo e impedir seguir retirando.
+
