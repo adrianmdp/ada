16d1fff1ea1b66716cb0b5fd9853794fd978570e
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:03:55 -0300

    Add ej 09

---
+++ javascript/clase66/ejercicios/09-esParOImpar/script.js
@@ -0,0 +1,10 @@
+// Crear una función esParOImpar que acepte como argumento un numero y devuelva el string par si el numero es par, o el string impar si el numero es impar
+
+
+
+console.log(esParOImpar(3)); // 'impar'
+console.log(esParOImpar(10)); // 'par'
+console.log(esParOImpar(31)); // 'impar'
+console.log(esParOImpar(98)); // 'par'
+console.log(esParOImpar(55)); // 'impar'
+console.log(esParOImpar(1042)); // 'par'
