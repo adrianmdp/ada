107ee7d962b9b52a7f3d396f2dce2df473d9f770
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 12:43:45 -0300

    add ej 08

---
+++ javascript/clase66/ejercicios/08-puedeGraduarse/script.js
@@ -0,0 +1,10 @@
+// Crear una función puedeGraduarse que tome como argumentos dos números asistencia y materiasAprobadas y un booleano tesisAprobada, y devuelva true si una persona puede gruadarse o false si no. Una persona puede graduarse si tiene un 75% de asistencia o más, 50 materias aprobadas o más y la tesis aprobada.
+
+puedeGraduarse(80, 50, true)  // true
+puedeGraduarse(80, 50, false) // false
+puedeGraduarse(80, 45, true)  // false
+puedeGraduarse(80, 45, false) // false
+puedeGraduarse(65, 50, true)  // false
+puedeGraduarse(33, 55, false) // false
+puedeGraduarse(42, 45, true)  // false
+puedeGraduarse(28, 45, false) // false
