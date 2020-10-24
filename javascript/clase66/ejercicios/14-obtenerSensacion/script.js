6a6b1262ca89df0025c6ec000d2e3b049f89a807
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 14:22:53 -0300

    add ej 14

---
+++ javascript/clase66/ejercicios/14-obtenerSensacion/script.js
@@ -0,0 +1,7 @@
+// Crear una función obtenerSensacion que tome como argumento un número temperatura y devuelva un string dependiendo de la temperatura, con las siguientes reglas:
+
+// Menor a 0°	                            => ¡Está helando!
+// Mayor o igual a 0° y menor a 15°	        => ¡Hace frío!
+// Mayor o igual a 15° y menor a 25°	    => Está lindo
+// Mayor o igual a entre 25° y menor a 30°	=> Hace calor
+// Mayor o igual de 30°	                    => ¡Hace mucho calor!
