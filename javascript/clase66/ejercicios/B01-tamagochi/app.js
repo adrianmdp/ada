b9863ddaed83c6d9b393aeecbe444b23847cb3f6
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:07:41 -0300

    Add ej B01

---
+++ javascript/clase66/ejercicios/B01-tamagochi/app.js
@@ -0,0 +1,12 @@
+/*
+Crear un programa que simule un Tamagochi (mascota virtual). Debe tener 4 variables: salud, felicidad, limpieza, energía, cuyos valores pueden ir entre 1 y 10. El programa debe iniciar mostrando un mensaje con el nombre del tamagochi y los valores de cada variable (que son asignados aleatoriamente). Luego, debe mostrar las siguientes posibles acciones a realizar, que modifican los valores de la siguiente forma:
+ALIMENTAR: +3 energía, +2 felicidad
+JUGAR: +2 felicidad, -2 energía, -1 limpieza
+DORMIR: +5 energía, +2 salud, -2 limpieza
+VACUNAR: +5 salud, -6 felicidad
+BAÑAR: +3 salud
+RETAR: -3 felicidad
+ACARICIAR: +4 felicidad
+Cuando el usuario selecciona una, debe mostrar el valor final de las variables.
+*/
+
