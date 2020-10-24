4cb01d700e8a1216f97f0ed6027e5505870f59d2
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:11:06 -0300

    Add ej 01

--- javascript/clase66/ejercicios/01-puedeVerPelicula/script.js
+++ javascript/clase66/ejercicios/01-puedeVerPelicula/script.js
@@ -1,1 +1,7 @@
 // Crear una función puedeVerPelicula que tome como argumentos un número edad y un booleano tieneAutorizacion, y devuelva true si la persona está habilitada para ver la película o false si no. Sólo puede ver la película si: tiene 15 años o más, o tiene autorización de sus padres.
+
+
+console.log(puedeVerPelicula(12, false)); 
+console.log(puedeVerPelicula(12, true)); 
+console.log(puedeVerPelicula(16, false));
+console.log(puedeVerPelicula(18, true)); 
