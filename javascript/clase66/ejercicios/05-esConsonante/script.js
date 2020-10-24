a35fc0052978404d321d6ef55b789f419b34bd12
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:03:05 -0300

    Add ej 05

---
+++ javascript/clase66/ejercicios/05-esConsonante/script.js
@@ -0,0 +1,8 @@
+// Crear una función esConsonante que tome como argumento un string letra y devuelva true si letra es una consonante o false si no lo es
+
+
+console.log(esConsonante('a')); // false
+console.log(esConsonante('n')); // true
+console.log(esConsonante('i')); // false
+console.log(esConsonante('e')); // false
+console.log(esConsonante('r')); // true
