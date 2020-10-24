9beb126875b835cf48ab10f331777251d8967499
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:10:33 -0300

    Add ej 02

--- javascript/clase66/ejercicios/02-estaEnRango/script.js
+++ javascript/clase66/ejercicios/02-estaEnRango/script.js
@@ -1,2 +1,8 @@
 // Crear una función estaEnRango que tome como argumentos tres números, un valor, un número minimo y un número maximo, y devuelva true si el valor se encuentra entre los números minimo y maximo o false si no (si el valor es igual a uno de los extremos se considera que está dentro del rango)
 
+
+console.log(estaEnRango(3, 1, 10));  // true
+console.log(estaEnRango(1, 1, 10));  // true
+console.log(estaEnRango(10, 1, 10)); // true
+console.log(estaEnRango(12, 1, 10)); // false
+console.log(estaEnRango(-3, 1, 10));  // false
