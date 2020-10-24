d2c75ac19299bc7d2af9bc9d0f2cb46d7d73f386
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 12:44:55 -0300

    add ej 10

---
+++ javascript/clase66/ejercicios/10-esPositivoONegativo/script.js
@@ -0,0 +1,4 @@
+// Crear una función esPositivoONegativo que acepte como argumento un numero y devuelva el string positivo si el numero es positivo, o el string negativo si el numero es negativo
+
+esPositivoONegativo(3)  // 'positivo'
+esPositivoONegativo(-5) // 'negativo'
