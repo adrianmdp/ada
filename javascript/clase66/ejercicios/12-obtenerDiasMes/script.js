c98580d203c80f3b4aa48a285a3fe36296405f6a
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 14:14:37 -0300

    add ej 12

---
+++ javascript/clase66/ejercicios/12-obtenerDiasMes/script.js
@@ -0,0 +1,4 @@
+// Crear una función obtenerDiasMes que tome como argumento un string mes y devuelva un número dependiendo de la cantidad de días que tenga ese mes
+
+obtenerDiasMes("diciembre") // 31
+obtenerDiasMes("febrero")   // 29
