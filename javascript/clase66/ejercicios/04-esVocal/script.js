595e3e70937b6e4f368bbdb6be0e3722ce6202ce
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 01:02:31 -0300

    Add ej 04

---
+++ javascript/clase66/ejercicios/04-esVocal/script.js
@@ -0,0 +1,13 @@
+// Crear una función esVocal que tome como argumento un string letra y devuelva true si letra es una vocal o false si no lo es.
+
+
+
+
+console.log(esVocal('a')); // true
+console.log(esVocal('n')); // false
+console.log(esVocal('e')); // true
+console.log(esVocal('f')); // false
+console.log(esVocal('u')); // true
+console.log(esVocal('i')); // true
+
+
