f5c2d636015fc6bdb6f27318e6259a06cc8c5fbc
Adrián Solimano <jasmdp@hotmail.com>
2020-10-23 22:51:55 -0300

    (improve) cambios no relevantes

--- javascript/clase65/js/script.js
+++ javascript/clase65/js/script.js
@@ -1,20 +1,14 @@
 /**
- * Crear una función puedeVerPelicula que tome como argumentos un número edad y un booleano tieneAutorizacion, y devuelva true si la persona está habilitada para ver la película o false si no. Sólo puede ver la película si: tiene 15 años o más, o tiene autorización de sus padres.
+    Crear una función burlarse que tome como argumentos un número edad y un booleano tieneAutorizacion, y devuelva true si la persona está habilitada para ver la película o false si no. Sólo puede ver la película si: tiene 15 años o más, o tiene autorización de sus padres.
  */
 
 // const burlarse = (str) => {
-
 //     const vocales = ["a", "e", "o", "u"];
-
 //     for(let i = 0; str.length > i; i++) {
-
 //         if(vocales.includes(str[i])){
-
 //             str = str.replace(str[i], 'i');
-
 //         }
 //     }
-
 //     return str;
 // }
 
