068abc0d86032ffc85d0be676016480fe9fdacae
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 00:52:59 -0300

    Add ej 03

---
+++ javascript/clase66/ejercicios/03-puedeAvanzar/script.js
@@ -0,0 +1,2 @@
+// Crear una función puedeAvanzar que tome como argumento un string con el color del semáforo y devuelva true si puede avanzar o false si no. Si no se ingresa un color válido, debe devolver un string que diga: Error: color de semáforo inválido
+
