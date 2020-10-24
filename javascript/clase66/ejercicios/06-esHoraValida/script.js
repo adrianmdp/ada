db14f7822c6d149ea5bb555a3debe564189f3909
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 12:41:43 -0300

    add ej 06

---
+++ javascript/clase66/ejercicios/06-esHoraValida/script.js
@@ -0,0 +1,6 @@
+// Crear una función esHoraValida que tome como argumento un string hora con el formato HH:mm y determine si es una hora válida del día o no
+
+esHoraValida('12:23') // true
+esHoraValida('12:65') // false
+esHoraValida('28:05') // false
+esHoraValida('00:00') // true
