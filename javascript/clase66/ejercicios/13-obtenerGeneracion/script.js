5c81c1d3db398e34a698302614dc1483ad9f8b37
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 14:20:21 -0300

    add ej 13

---
+++ javascript/clase66/ejercicios/13-obtenerGeneracion/script.js
@@ -0,0 +1,6 @@
+// Crear una función obtenerGeneracion que tome como argumento un número anioNacimiento y devuelva un string con la generación a la que pertenece, siguientdo estas reglas:
+
+// Baby boomer	    => 1949-1968
+// Generación X	    => 969-1980
+// Millennials	    => 1981-1993
+// Generación Z	    => 1994-2010
