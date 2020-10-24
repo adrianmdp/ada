79365661896e8c10bd13a2754d201fa0af6bff9f
Adrián Solimano <jasmdp@hotmail.com>
2020-10-24 12:45:44 -0300

    add ej 11

---
+++ javascript/clase66/ejercicios/11-avanzarSemaforo/script.js
@@ -0,0 +1,5 @@
+// Crear una función avanzarSemaforo que acepte como argumento un string colorActual y devuelva un string con el siguiente color del semáforo, siguiendo el orden: verde -> amarillo -> rojo -> verde
+
+avanzarSemaforo('verde')     // 'amarillo'
+avanzarSemaforo('amarillo')  // 'rojo'
+avanzarSemaforo('rojo')      // 'verde'
