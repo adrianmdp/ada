94debe3ab561fc31b7a699c695c4198ffe5639ff
Adrián <adriansolimano@gmail.com>
2020-10-27 22:03:00 -0300

    corregido error en el ej 1

--- javascript/clase67/ejercicios/01-obtenerMenor/script.js
+++ javascript/clase67/ejercicios/01-obtenerMenor/script.js
@@ -1,3 +1,3 @@
 // Crear una función obtenerMenor que tome como argumento un array de números numeros y devuelva el menor de ellos. Ejemplo:
 
-obtenerNumeroMenor(5, 7, 99, 34, 54, 2, 12); // 2
+obtenerNumeroMenor([5, 7, 99, 34, 54, 2, 12]); // 2
