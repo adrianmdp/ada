d063986811969e70cfa59bee2ac4fa912c5dbb0a
adrianmdp <jasmdp@hotmail.com>
2020-10-31 13:22:26 -0300

    Update script.js

--- javascript/clase67/ejercicios/07-estanJuntos/script.js
+++ javascript/clase67/ejercicios/07-estanJuntos/script.js
@@ -1,5 +1,5 @@
 // Crear una función estanJuntos que tome como argumento un array de strings personajes, y devuelva true si Sam se encuentra al lado de Frodo, ya sea antes o después, o false sino. Ejemplo:
 
 estanJuntos(["Sam", "Frodo", "Legolas"]); //true
-estanJuntos(["Aragorn", "Frodo", "Frodo"]); //true
-estanJuntos(["Sam", "Orco", "Frodo"]); //true
+estanJuntos(["Aragorn", "Frodo", "Frodo"]); //false
+estanJuntos(["Sam", "Orco", "Frodo"]); //false
