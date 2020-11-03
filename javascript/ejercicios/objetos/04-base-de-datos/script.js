156bf7125c57f759cfdab2f0a5f951c91f0eb635
Adrián <adriansolimano@gmail.com>
2020-11-03 20:40:05 -0300

    Nuevos ejercicios

---
+++ javascript/ejercicios/objetos/04-base-de-datos/script.js
@@ -0,0 +1,32 @@
+// Nos pasaron un listado de personas y necesitamos guardarlo en una base de datos
+// Los datos que tenemos son un id, email, nombre y teléfono
+// Crear un objeto para cada persona que nos pasaron
+// Mostrar por consola todos los objetos creados
+
+// Ejemplo del formato de cada objeto: 0,ejemplo@terra.com,Ejemplo,1029384756
+// var example = {
+//   id: 0,
+//   nombre: "Ejemplo",
+//   email: "ejemplo@terra.com",
+//   telefono: "1029384756"
+// }
+
+// Escribi aca abajo los objetos para cada uno de las personas que nos enviaron
+
+// Nos pidieron mostrar en la consola los siguientes datos de cada persona:
+//// TENEMOS QUE MOSTRAR LOS SIGUIENTES DATOS
+
+// 1. El nombre de Ada:
+console.log();
+
+// 2. El ID de Grace
+console.log();
+
+// 3. El email de Hedy
+console.log();
+
+// 4. El ID y nombre de Radia
+console.log();
+
+// 5. El telefono de Sheryl
+console.log();
