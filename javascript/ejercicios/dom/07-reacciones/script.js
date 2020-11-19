b16de30e29c53f5c0a523acd87ea8b4fba9eb0c8
Adrián <adriansolimano@gmail.com>
2020-11-19 20:35:44 -0300

    Nuevos ejercicios

---
+++ javascript/ejercicios/dom/07-reacciones/script.js
@@ -0,0 +1,10 @@
+// En un documento html hacer un post de una red social que contenga:
+
+// un h3 con la usuaria
+// un p con un lorem ipsum
+// 3 spans de reacciones con un ícono (por ejemplo Me gusta, Me encanta, Me asombra) más un número con la cantidad
+// Dar estilos para que
+
+// la tipografía sea distinta a la default
+// los spans estén en línea, separados con márgenes, tengan un color de fondo gris suave y un border radius
+// Hacer un programa que al iniciarse pregunte mediante tres prompts por la cantidad de cada una de las reacciones y actualice los valores de los spans correspondientes
