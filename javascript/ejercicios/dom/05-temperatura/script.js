eeed6c33acb9749a36755fa46cbbd0d08a9b97c3
Adrián <adriansolimano@gmail.com>
2020-11-19 20:31:02 -0300

    Nuevos ejercicios

---
+++ javascript/ejercicios/dom/05-temperatura/script.js
@@ -0,0 +1,9 @@
+// Hacer un programa que al iniciarse pregunte mediante un prompt por un temperatura, y con ese dato modifique un h1 en el html, de forma tal que diga, por ejemplo, Temperatura actual: 20°. Cambiar el color del h1 dependiendo de la temperatura ingresada, siguiendo las siguientes reglas:
+
+// Temperatura	Color
+// Menor a 0°	                        => Azul
+// Mayor o igual a 0° y menor a 15°	    => Celeste
+// Mayor o igual a 15° y menor a 25°	=> Verde
+// Mayor o igual a 25° y menor a 30°	=> Amarillo
+// Mayor o igual a 30° y menor a 35°	=> Naranja
+// Mayor a 35°                          => Rojo
