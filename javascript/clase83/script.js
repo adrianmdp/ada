397b491df527b9509d4e95db19ed8457ce268ce0
Adrián Solimano <adriansolimano@gmail.com>
2020-12-12 03:11:55 -0300

    Ejercicios agregados

--- javascript/clase83/script.js
+++ javascript/clase83/script.js
@@ -1,10 +1,10 @@
 const root = "https://api.themoviedb.org/3/";
 const apiKey = "0bce2b3619ac231ad6ce364b6d8015ab";
 
-const url = new URL("movie/top_rated", root);
+// const url = new URL("movie/top_rated", root);
 
-url.searchParams.set("api_key", apiKey);
+// url.searchParams.set("api_key", apiKey);
 
-fetch(url).then((response) => {
-  console.log(response);
-});
+// fetch(url).then((response) => {
+//   console.log(response);
+// });
